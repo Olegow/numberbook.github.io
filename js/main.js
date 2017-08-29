a54290c5e74a09ef6a6072831ca78eeefe02f8cd
olegow <belobrov.oleg12@gamil.com>
2017-08-29 17:01:23 +0300

    option1

---
+++ js/main.js
@@ -0,0 +1,59 @@
+;(function() {
+
+  var addNumBut = document.getElementById('add_num_but');
+  window.showAddNum = function () {
+    var add_num = document.getElementById('add_num');
+    add_num.style.display = 'flex';
+    setTimeout(function() {
+      add_num.style.opacity='1'
+    },5);
+  }
+
+  var numbers = [];
+
+  window.getPersonInfo = function () {
+    var firstname = document.getElementById('firstname');
+    var lastname = document.getElementById('lastname');
+    var number = document.getElementById('number');
+    var email = document.getElementById('email');
+    var firstnameVal = firstname.value;
+    var lastnameVal = lastname.value;
+    var numberVal = number.value;
+    var emailVal = email.value;
+    var numberList = document.getElementById('numberList');
+      if (firstnameVal != "" && lastnameVal != "" && numberVal != "" && emailVal != "") {
+
+
+          for (var i = 0; i < numbers.length; i++) {
+              var person = {
+                  firstname: firstnameVal,
+                  lastname: lastnameVal,
+                  number: [numberVal],
+                  email: emailVal
+              };
+
+              numbers.push(person);
+
+
+              localStorage.setItem('number' + [i], JSON.stringify(person[i]));
+              var entry = document.createElement('li');
+              entry.appendChild(document.createTextNode(localStorage.getItem(person, firstname)));
+              var editButton = document.createElement('button');
+              editButton.appendChild(document.createTextNode("Изменить"));
+              editButton.setAttribute('onClick','editName('+i+')');
+              entry.appendChild(editButton);
+              numberList.appendChild(entry);
+          }
+
+
+        var add_num = document.getElementById('add_num');
+        add_num.style.display = 'none';
+        setTimeout(function() {
+          add_num.style.opacity='0'
+        },5);
+      }
+
+
+  }
+
+})();
